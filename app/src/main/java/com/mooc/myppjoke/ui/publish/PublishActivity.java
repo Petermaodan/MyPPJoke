4f05cbba4f8106d4efa8ef662e3fc9aeab6d620d
Petermaodan <616673978@qq.com>
2021-08-19 11:46:47 +0800

    底部导航栏构建

---
+++ app/src/main/java/com/mooc/myppjoke/ui/publish/PublishActivity.java
@@ -0,0 +1,16 @@
+package com.mooc.myppjoke.ui.publish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mooc.myppjoke.R;
+
+public class PublishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+    }
+}
